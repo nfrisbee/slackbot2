f2de19e954987644858249339ef90512c6991414
nfrisbee <nicolefrisbee@yahoo.com>
2019-04-23 15:33:25 -0600

    added API call and POST

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const Slackbot = require('slackbots');
 const axios = require('axios');
-const env = require('dotenv');
-env.load();
-
+const url = `https://api.flowroute.com/v2/portorders/portability`;
 const bot = new Slackbot({
-    token: 'xoxb-580618039601-590779891974-vji2kzD7ErMYycc2WeAdJSqM',
+    token: 'xoxb-580618039601-590779891974-UBwwfBWR0k6miQzC53HFYzS3',
     name: 'portbot'
 });
 
@@ -27,7 +25,7 @@ bot.on('error', (err) => console.log(err));
 // Message Handler
 bot.on('message', data => {
     if (data.type !== 'message' || data.subtype === 'bot_message') {
-        return;
+        return; 
     }
 
     handleMessage(data.text);
@@ -35,43 +33,64 @@ bot.on('message', data => {
 
 // Response to Data
 function handleMessage(message) {
-    if(message.includes('portability')) {
+    if(message.includes('hello')) {
         portcheck();
     }
     else if(message.includes('portable')) {
         portcheck();
     }
     else{
-         notvalid();
+         notvalid();     
     }
 };
 // Portability Responses
 function portcheck() {
-    axios.get('http://api.yomomma.info')
+    axios.post(`https://api.flowroute.com/v2/portorders/portability`)
+    axios({
+        method: 'post',
+        url,
+        auth: {
+          username: '95811327',
+          password: 'KVVWeZPf9YkbrPcp15gutZmAM9JD0d0o'
+        }
+    })
     .then(res => {
-      const joke = res.data.joke;
-
+      const portable = res.data.portable;
+      
       const params = {
         icon_emoji: ':tada:'
        };
-
+    
        bot.postMessageToChannel(
            'portbot',
-           `Yo Momma: ${joke}`, params);
+           `Status: ${portable}`, params);
+    })
+    .catch(function (error) {
+        console.log(error);
     });
-}
+};
 
 //If not valid message
 function notvalid() {
     const noParam = {
         icon_emoji: ':laughing:'
        };
-
+    
        bot.postMessageToChannel(
            'portbot',
-           'What would you like me to do?' ,
+           'What would you like me to do? Give me a 10 digit number to check portability.' ,
            noParam,
          );
-
+         
 
 };
+
+//Axios 
+// axios({
+//     method: 'post',
+//     url: 'https://api.flowroute.com/v2/portorders/portability',
+//     auth: {
+//       username: ' ',
+//       password: ' '
+//     }
+//   });
